99698e5784ee12d74090ee2cd3975486e454ef45
yehuda.zfati <yehuda.zfati@gmail.com>
2025-12-31 09:24:12 +0000

    use scss file

--- src/app/calendar/calendar.ts
+++ src/app/calendar/calendar.ts
@@ -2,13 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'calendar',
-  imports: [],
   template: `
     <ng-content select='calendar-navigator'></ng-content>
     <ng-content select='calendar-month'></ng-content>
   `,
   styles: ``,
 })
-export class Calendar {
-
-}
+export class Calendar {}
